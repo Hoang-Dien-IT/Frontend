b20df8c18c2f260a5d2a8cd2eea69a07a7b7c152
Hoang-Dien-IT <nguyenhoangdien1x@gmail.com>
2024-11-01 01:27:22 +0700

    Tao trang them

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="page">
+        <h4>Thêm mới Liên hệ</h4>
+        <ContactForm :contact="contact" @submit:contact="addContact" />
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+
+export default {
+    components: {
+        ContactForm,
+    },
+    data() {
+        return {
+            contact: { name: '', email: '', phone: '' }, // Khởi tạo một đối tượng liên hệ mới
+            message: "",
+        };
+    },
+    methods: {
+        async addContact(data) {
+            try {
+                await ContactService.create(data); // Gọi API để thêm liên hệ mới
+                alert('Liên hệ được thêm thành công.');
+                this.$router.push({ name: "contactbook" });
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
+};
+</script>
